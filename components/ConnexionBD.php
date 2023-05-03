ba316cecce8515aaf381e145ae32b2013ac16efc
Olfa Medimegh <olfa.medimegh@insat.ucar.tn>
2023-05-03 22:49:27 +0100

    **

--- components/ConnexionBD.php
+++ components/ConnexionBD.php
@@ -1,12 +1,32 @@
-<?php class ConnexionBD
-{
+<?php 
+
+    $db_name = 'mysql:host=localhost;dbname=home_db';
+    $db_user_name = 'root';
+    $db_user_pass = '';
+
+
+    $conn = new PDO($_dbname, $_user , $db_user_pass);
+
+    function create_unique_id(){
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < 20; $i++) {
+        $randomString .= $characters[mt_rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+
+    }
+?>
+
+/*  Class ConnexionBD
     private static $_dbname = "home_db";
     private static $_user = "root";
     private static $_pwd = "";
     private static $_host = "localhost";
     private static $_bdd = null;
 
-    private function __construct()
+   private function __construct()
     {
         try {
             self::$_bdd = new PDO("mysql:host=" . self::$_host . ";dbname=" . self::$_dbname . ";charset=utf8", self::$_user, self::$_pwd, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
@@ -22,6 +42,4 @@
         }
         return (self::$_bdd);
     }
-
-}
-?>
+ */
--- login.php
+++ login.php
@@ -1,15 +1,34 @@
 <!-- php
-    include 'components/ConnexionBD.php';
-    if (isset($_COOKIE[user_id])){
-        $user_id = $_COOKIE[user_id];
-    }else{
-        $user_id = '';
-    }
 
-    if (isset($_POST['submit'])){
-        $email = $_POST['email'];
-    }
-  -->
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+if(isset($_POST['submit'])){
+
+   $email = $_POST['email'];
+   $email = filter_var($email, FILTER_SANITIZE_STRING); 
+   $pass = sha1($_POST['pass']);
+   $pass = filter_var($pass, FILTER_SANITIZE_STRING); 
+
+   $select_users = $conn->prepare("SELECT * FROM `users` WHERE email = ? AND password = ? LIMIT 1");
+   $select_users->execute([$email, $pass]);
+   $row = $select_users->fetch(PDO::FETCH_ASSOC);
+
+   if($select_users->rowCount() > 0){
+      setcookie('user_id', $row['id'], time() + 60*60*24*30, '/');
+      header('location:home.php');
+   }else{
+      $warning_msg[] = 'Incorrect username or password!';
+   }
+
+}
+
+ -->
 
 <!DOCTYPE html>
 <html lang="en">
