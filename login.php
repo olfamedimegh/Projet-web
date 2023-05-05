7801db56f0dc5c0a50cf733dd7e6aac34517badd
Olfa Medimegh <olfa.medimegh@insat.ucar.tn>
2023-05-05 12:09:43 +0100

    search

--- login.php
+++ login.php
@@ -38,7 +38,7 @@ if(isset($_POST['submit'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style_login.css">
 
 </head>
 <body>
--- search.php
+++ search.php
@@ -1,14 +1,16 @@
 <!-- php  
 
-include 'components/ConnexionBD.php';
+include 'components/ConnectionBD.php';
 
 if(isset($_COOKIE['user_id'])){
    $user_id = $_COOKIE['user_id'];
 }else{
    $user_id = '';
-} -->
+}
 
+include 'components/save_send.php';
 
+?> -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -16,51 +18,65 @@ if(isset($_COOKIE['user_id'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>Search Page</title>
 
-    <link rel="stylesheet" href="css/style_home.css">
+     <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
 
 </head>
 <body>
     
+<?php include 'components/user_header.php'; ?>
 
-<!--header  --> 
+    <!-- search filter section starts -->
+    
+<section class="filters" style="padding-bottom: 0;">
 
-<?php include 'components/user_header.php' ?>
-
-
-<!-- home section -->
-
-<div class="home">
-<!-- search box-->
-<section class="center">
-
-   <form action="search.php" method="post">
-      <h3>find your perfect home</h3>
-      <div class="box">
-         <p>Enter location <span>*</span></p>
-         <input type="text" name="h_location" required maxlength="100" placeholder="Enter city name" class="input">
-      </div>
+<form action="" method="post">
+   <div id="close-filter"><i class="fas fa-times"></i></div>
+   <h3>filter your search</h3>
+      
       <div class="flex">
          <div class="box">
-            <p>Property Type <span>*</span></p>
-            <select name="h_type" class="input" required>
+            <p>Enter location</p>
+            <input type="text" name="location" required maxlength="100" placeholder="Enter city name" class="input">
+         </div>
+         <div class="box">
+            <p>Offer type</p>
+            <select name="offer" class="input" required>
+               <option value="sale">sale</option>
+               <option value="rent">rent</option>
+            </select>
+         </div>
+         <div class="box">
+            <p>Property Type</p>
+            <select name="type" class="input" required>
                <option value="flat">flat</option>
                <option value="house">house</option>
                <option value="shop">shop</option>
             </select>
          </div>
          <div class="box">
-            <p>Offer type <span>*</span></p>
-            <select name="h_offer" class="input" required>
-               <option value="sale">sale</option>
-               <option value="rent">rent</option>
+            <p>how many BHK</p>
+            <select name="bhk" class="input" required>
+               <option value="1">1 BHK</option>
+               <option value="2">2 BHK</option>
+               <option value="3">3 BHK</option>
+               <option value="4">4 BHK</option>
+               <option value="5">5 BHK</option>
+               <option value="6">6 BHK</option>
+               <option value="7">7 BHK</option>
+               <option value="8">8 BHK</option>
+               <option value="9">9 BHK</option>
             </select>
          </div>
          <div class="box">
-            <p>Min Price DT <span>*</span></p>
-            <select name="h_min" class="input" required>
+            <p>Min Price DT</p>
+            <select name="min" class="input" required>
                <option value="100">100</option>
                <option value="250">250</option>
                <option value="500">500</option>
@@ -89,8 +105,8 @@ if(isset($_COOKIE['user_id'])){
             </select>
          </div>
          <div class="box">
-            <p>Max Price DT <span>*</span></p>
-            <select name="h_max" class="input" required>
+            <p>Max Price DT</p>
+            <select name="max" class="input" required>
                <option value="100">100</option>
                <option value="250">250</option>
                <option value="500">500</option>
@@ -116,89 +132,34 @@ if(isset($_COOKIE['user_id'])){
                <option value="800000">800 milion</option>
                <option value="900000">900 milion</option>
                <option value="1000000">1 bilion</option>
+            </select>
+         </div>
+         <div class="box">
+            <p>status</p>
+            <select name="status" class="input" required>
+               <option value="ready to move">ready to move</option>
+               <option value="under construction">under construction</option>
+            </select>
+         </div>
+         <div class="box">
+            <p>furnished</p>
+            <select name="furnished" class="input" required>
+               <option value="unfurnished">unfurnished</option>
+               <option value="furnished">furnished</option>
+               <option value="semi-furnished">semi-furnished</option>
             </select>
          </div>
       </div>
-      <input type="submit" value="search property" name="h_search" class="btn">
-   </form>
-
-</section>
-</div>
-
-
-<!-- services section  -->
-
-<section class="services">
-
-<h1 class="heading">our services</h1>
-
-<div class="box-container">
-
-   <div class="box">
-      <img src="images/icon-1.png" alt="">
-      <h3>buy house</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, incidunt.</p>
-   </div>
-
-   <div class="box">
-      <img src="images/icon-2.png" alt="">
-      <h3>rent house</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, incidunt.</p>
-   </div>
-
-   <div class="box">
-      <img src="images/icon-3.png" alt="">
-      <h3>sell house</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, incidunt.</p>
-   </div>
-
-   <div class="box">
-      <img src="images/icon-4.png" alt="">
-      <h3>flats and buildings</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, incidunt.</p>
-   </div>
-
-   <div class="box">
-      <img src="images/icon-5.png" alt="">
-      <h3>shops and malls</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, incidunt.</p>
-   </div>
-
-   <div class="box">
-      <img src="images/icon-6.png" alt="">
-      <h3>24/7 service</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, incidunt.</p>
-   </div>
-
-</div>
+      <input type="submit" value="search property" name="filter_search" class="btn">
+</form>
 
 </section>
 
-<!-- listings section starts  -->
+    <!-- search filter section ends -->
+     <!-- partie php -->
 
-<section class="listings">
+     <!-- listings section starts  -->
 
-<h1 class="heading">latest listings</h1>
-<div class="box-container">
-</div>
-
-</section>
-
-<!-- alert cdn link-->
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
-
-<!-- footer section  -->
-<?php include 'components/footer.php'; ?>
-
-
-<!-- js file link-->
-<script src ="js/script.js"></script>
-
-<?php include 'components/message.php' ?>
 
 </body>
-
-
-
 </html>
